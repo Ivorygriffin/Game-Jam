9e9c033c99f7b5b3011498a5a1f22d63dfb11350
Ivorygriffin <s210559@students.aie.edu.au>
2022-04-18 22:59:48 +0930

    script edits

--- Game Jam/Assets/Scripts/GameController.cs
+++ Game Jam/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public enum CampType 
 {
     socialising,
@@ -25,9 +26,7 @@ public enum ItemType
 
 public class GameController : MonoBehaviour
 {
-    [Header("enums")]
-    public CampType campType;
-    public ItemType itemType;
+    
 
     [Header("Text")]
     public TMP_Text ScoreText;
@@ -35,15 +34,34 @@ public class GameController : MonoBehaviour
     public TMP_Text PointsUntilNextCamp;
 
     [Header("Ints")]
-    public int currentScore;
+    
     public int camp1Score;
     public int camp2Score;
     public int finalCampScore;
     public int alcoholVal, appleVal, teaVal, sleepVal, candyVal, tvVal;
     
+
+    [Header("GameObjects")]
+    public GameObject camp1,camp2;
+    public GameObject slider1, slider2, slider3;
+
+    [Header("Sliders")]
+    public Slider s1, s2, s3;
+
+    [Header("floats")]
+    public float s1Max;
+    public float s2Max;
+    public float s3Max;
+    public float currentScore;
+
+    [Header("bools")]
+    private bool _slider1, _slider2, _slider3;
+    
     void Start()
     {
-        
+        _slider1 = true;
+        _slider2 = false;
+        _slider3 = false;
     }
 
     
@@ -52,6 +70,7 @@ public class GameController : MonoBehaviour
         ScoreTextUpdate();
         MountainClimbing();
         PointCheck();
+        
     }
 
     public void ScoreTextUpdate()
@@ -59,34 +78,45 @@ public class GameController : MonoBehaviour
         ScoreText.text = currentScore.ToString();
     }
 
+  
+
+
+
+
     public void MountainClimbing()
     {
         // UI person climbs more depending on score collected
-    }
-
-    public void CampReached()
-    {
-        switch (campType)
+        //slider moves stick figure up 
+        if (_slider1 == true && currentScore != s1Max)
         {
-            case CampType.socialising:
-                Socialising();
-                Debug.Log("socialising");
-                break;
-
-            case CampType.relaxing:
-                Relaxing();
-                Debug.Log("relaxing");
-                break;
-
-            default:
-                Debug.Log("campType not set");
-                break;
-
-                //this wrong 
+            s1.value = currentScore;
+        }
+        if (currentScore == s1Max && _slider1 == true)
+        {
+            slider1.SetActive(false);
+            _slider1 = false;
+            slider2.SetActive(true);
+            _slider2 = true;
+        }
+        if(_slider2 == true && currentScore != s2Max)
+        {
+            s2.value = currentScore;
+        }
+        if (currentScore == s2Max && _slider2 == true)
+        {
+            slider2.SetActive(false);
+            _slider2 = false;
+            slider3.SetActive(true);
+            _slider3 = true;
+        }
+        if(_slider3 == true && currentScore != s3Max)
+        {
+            s3.value = currentScore;
         }
 
-  
     }
+
+   
 
     public void Socialising()
     {
@@ -101,6 +131,10 @@ public class GameController : MonoBehaviour
     public void LevelComplete()
     {
         //third camp reached triggers end of level 
+        if(currentScore == finalCampScore)
+        {
+            Debug.Log("levelComplete");
+        }
     }
 
     public void GameOver()
@@ -111,61 +145,28 @@ public class GameController : MonoBehaviour
     public void ItemHit()
     {
         //item collision determines number of points added
-        switch (itemType) 
-        {
-            case ItemType.alcohol:
-                currentScore += alcoholVal;
-                Debug.Log("alcohol");
-                break;
-
-            case ItemType.apple:
-                currentScore += appleVal;
-                Debug.Log("apple");
-                break;
-
-            case ItemType.candy:
-                currentScore += candyVal;
-                Debug.Log("candy");
-                break;
-
-            case ItemType.fastFood:
-                SuperBad();
-                Debug.Log("fast");
-                break;
-
-            case ItemType.sleep:
-                currentScore += sleepVal;
-                Debug.Log("sleep");
-                break;
-
-            case ItemType.tea:
-                currentScore += teaVal;
-                Debug.Log("tea");
-                break;
-
-            case ItemType.tv:
-                currentScore += tvVal;
-                Debug.Log("tv");
-                break;
-
-            case ItemType.water:
-                SuperGood();
-                Debug.Log("water");
-                break;
-
-            default:
-                Debug.Log("not item type assigned");
-                break;
-
-
-
-        }
+       
 
     }
 
     public void PointCheck()
     {
         //checks if number of points reaches a camp
+        if(currentScore == camp1Score)
+        {
+            camp1.SetActive(true);
+             // go into different script attached to camp in scene and check type
+        }
+        if(currentScore == camp2Score)
+        {
+            camp1.SetActive(false);
+            camp2.SetActive(true);
+            // go into different script attached to camp in scene and check type
+        }
+
+
+
+
     }
     public void SuperBad()
     {
